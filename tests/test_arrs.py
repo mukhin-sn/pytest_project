3d19ba3526f3371bd934bffe42085483f44fe803
Sergey Mukhin <senikomu@gmail.com>
2023-10-04 20:26:40 +0700

    test_slice = 100% and add test_slice_type_error()

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,23 @@
+import pytest
+
 from utils import arrs
 
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2], 1, "test") == 2
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4, 5], end=3) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], 2, 10) == [3, 4, 5]
+
+
+def test_slice_type_error():
+    with pytest.raises(TypeError):
+        arrs.my_slice(1259, 2, 3)
